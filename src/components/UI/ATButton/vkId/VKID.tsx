b2d6915b592f0b0ee9e623346be85b7afd44f868
Anton <popsof@yandex.ru>
2024-09-25 09:55:43 +0300

    fix

--- src/components/UI/ATButton/vkId/VKID.tsx
+++ src/components/UI/ATButton/vkId/VKID.tsx
@@ -6,31 +6,23 @@ const VKIDAuthComponent = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const store = new Store();
   const oneTapInitialized = useRef(false);
-
-  const [deviceId , setDeviceId] = useState<string>('')
+  const [deviceId, setDeviceId] = useState<string>('');
 
   const generateDeviceId = () => {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-        const r = (Math.random() * 16) | 0,
-          v = c === 'x' ? r : (r & 0x3) | 0x8;
-          
-          setDeviceId(v.toString(16))
-          return v.toString(16);
-        
-      });
-    };
-
-   
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      const r = (Math.random() * 16) | 0;
+      const v = c === 'x' ? r : (r & 0x3) | 0x8;
+      setDeviceId(v.toString(16));
+      return v.toString(16);
+    });
+  };
 
   useEffect(() => {
-
-    console.log("device:",deviceId)
-
-    generateDeviceId()
+    generateDeviceId();
 
     VKID.Config.init({
       app: '52336772',
-      redirectUrl: 'https://e35f-92-39-220-81.ngrok-free.app',
+      redirectUrl: 'https://main--transcendent-frangipane-30b77b.netlify.app/',
       scope: 'email phone',
     });
 
@@ -38,23 +30,19 @@ const VKIDAuthComponent = () => {
       const oneTap = new VKID.OneTap();
       oneTap.render({ container: containerRef.current });
       oneTapInitialized.current = true;
+
+      // Обработка результата авторизации
+    oneTap.on('login', (payload) => {
+      console.log('Payload:', payload);
+      store.loginWithVkId(payload, deviceId, handleError);
+    });
+
+    // Проверка ошибок авторизации
+    oneTap.on('error', (error) => {
+      console.error('Ошибка авторизации VK ID:', error);
+    });
     }
-
-    // Проверяем код авторизации в URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get('code');
-
-    if (code) {
-      console.log('Authorization code:', code);
-
-      // Удаляем код из URL после обработки
-      window.history.replaceState({}, document.title, window.location.pathname);
-
-      store.loginWithVkId(code, deviceId, handleError);
-    } else {
-      console.error('Authorization code not found in URL.');
-    }
-  }, []);
+  }, [store]);
 
   const handleError = (error: any) => {
     console.error('Ошибка авторизации VK ID:', error);
