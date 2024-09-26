36cb15dba3cc34ce18612d7b03d4ebf521fbf45d
Anton <popsof@yandex.ru>
2024-09-26 10:43:37 +0300

    fix

--- src/components/UI/ATButton/vkId/VKID.tsx
+++ src/components/UI/ATButton/vkId/VKID.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as VKID from '@vkid/sdk';
 import Store from '../../../store';
 
@@ -7,43 +7,47 @@ const VKIDAuthComponent = () => {
   const store = new Store();
   const oneTapInitialized = useRef(false);
 
-  VKID.Config.init({
-    app: '52336772', // Идентификатор приложения.
-    redirectUrl: 'https://main--transcendent-frangipane-30b77b.netlify.app', // Адрес для перехода после авторизации.
-    state: 'dj29fnsadjsd82', // Произвольная строка состояния приложения.
-    codeVerifier: 'FGH767Gd65', // Верификатор в виде случайной строки. Обеспечивает защиту передаваемых данных.
-    scope: 'email phone', // Список прав доступа, которые нужны приложению.
-
-  });
-
-  if (!oneTapInitialized.current && containerRef.current) {
-    const oneTap = new VKID.OneTap();
-    oneTap.render({ container: containerRef.current });
-    oneTapInitialized.current = true;
-  }
-
   useEffect(() => {
+    VKID.Config.init({
+      app: '52336772',
+      redirectUrl: 'https://main--transcendent-frangipane-30b77b.netlify.app/',
+      scope: 'email phone',
+    });
+
+    if (!oneTapInitialized.current && containerRef.current) {
+      const oneTap = new VKID.OneTap();
+      oneTap.render({ container: containerRef.current });
+      oneTapInitialized.current = true;
+    }
 
     // Проверяем код авторизации в URL
     const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get('code');
-    const deviceId = urlParams.get('device_id')
-    const state = urlParams.get('state')
-
+    const code = urlParams.get('device_id');
+    const deviceId = urlParams.get('device_id');
 
 
     if (code) {
-      console.log('Authorization code:', deviceId);
+      console.log('Authorization code:', code);
 
+      store.loginWithVk(code, handleError)
+      .then(() => {
+        console.log('Авторизация прошла успешно');
+        setTimeout(()=>{ 
+          window.location.href = '/'
+        },100)   
+      })
+      .catch((error: any) => {
+        console.error('Ошибка обработки авторизации', error);
+      });
       
     } else {
       console.error('Authorization code not found in URL.');
     }
   }, []);
 
-  // const handleError = (error: any) => {
-  //   console.error('Ошибка авторизации VK ID:', error);
-  // };
+  const handleError = (error: any) => {
+    console.error('Ошибка авторизации VK ID:', error);
+  };
 
   return (
     <div>
@@ -53,4 +57,3 @@ const VKIDAuthComponent = () => {
 };
 
 export default VKIDAuthComponent;
-
