975d69f84faa767485cf85ea8ef4db9e2be638b9
Anton <popsof@yandex.ru>
2024-09-26 17:45:15 +0300

    fix

--- src/components/UI/ATButton/vkId/VKID.tsx
+++ src/components/UI/ATButton/vkId/VKID.tsx
@@ -4,7 +4,7 @@ import * as VKID from '@vkid/sdk';
 const VKIDAuthComponent = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const oneTapInitialized = useRef(false);
-  const codeVerifier = 'FGH767Gd65dsf76TgBh98vGbvDsF7GhEtr67G4Rf'; // Должен быть одинаковым на клиенте и сервере
+  const codeVerifier = 'FGH767Gd65dsf76TgBh98vGbvDsF7GhEtr67ghjhuf'; // Должен быть одинаковым на клиенте и сервере
 
   useEffect(() => {
     VKID.Config.init({
